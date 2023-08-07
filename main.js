dfa60d9e7784192770ae86e4cd2ff5ec99e88b56
vgtario <139800025+vgtario@users.noreply.github.com>
2023-08-07 16:24:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+//calculos basicos com variavei e lógica
+
+//var idade, eleitor, resultado;
+//idade = 71
+//eleitor = (idade < 18)
+//resultado = (idade === 65);
+//alert (resultado); //alert é aquela aba em cima da barra de navegação
+
+//somando numeros com variaveis
+
+//var espectativa, tempovida, resultado
+//tempovida = 100
+//espectativa = 85
+//resultado = (tempovida - espectativa)
+//alert (resultado)
+
+
+//somando e subtraindo numeros com variaveis usando comando if else
+
+//var num, numero, resposta;
+//num = 45;
+//numero = 15;
+//if (num > numero) {
+ //   resposta = num - numero;
+ //   alert("A subtração de 'num' por 'numero' é: " + resposta); // para acresentar o resultado, ou resposta no alert(mensagem para o usuario) você deve ultilizar +var(resposta, resultado) dentro do alert//
+//} else if (num < numero) {
+  //  resposta = numero - num;
+  //  alert("A subtração de 'numero' por 'num' é: " + resposta);
+//} else {
+  //  resposta = "Os números são iguais, a subtração é 0.";
+  //  alert(resposta);
+//}
+
+//somando numeros com função
+
+
+//var num, numero, resposta;
+//num = 18;
+//numero = 29;
+//function calcularSubtracao(a, b) {// nome da função calcularSubtracao
+  //  return a - b;
+//}
+//resposta = calcularSubtracao(num, numero);// você deve acresentar o nome da função
+//alert("O resultado é: " + resposta);
+
+//somando numeros com função
+
+//function somar (a,b){
+    //var b = 50;
+   // var a = 20;
+  //  return (a-b);
+//}
+//alert (somar(50,20));//nesse caso voce deve usar o "resultado" de a e b dentro do alert
+
+//verificado se impar ou par um numero atraves do funciton e if else
+
+//function VerificarImparPar (numero) {
+//if (numero % 2 === 0) {
+//    console.log(numero + " e um numero orientation.");
+//} else {
+//    console.log(numero + " nao e um numero par.");
+//}
+//}
+//console.log (VerificarImparPar(255))//console também mostra o resultado assim como o alert
+
+//ícone || = ou, && = e
+
+//seilá
